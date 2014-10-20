61f721863c3cdf25f10fcbf8672242548caf6304
Ivorius <lukastenbrink@googlemail.com>
2014-10-20 20:27:51 +0200

    Add ivtoolkit-style class transformer

---
+++ src/main/java/mod/gravitycraft/core/GravityCraftClassTransformer.java
@@ -0,0 +1,13 @@
+package mod.gravitycraft.core;
+
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by lukas on 20.10.14.
+ */
+public class GravityCraftClassTransformer
+{
+    public GravityCraftClassTransformer()
+    {
+    }
+}
--- src/main/java/mod/gravitycraft/core/GravityCraftCoreLoader.java
+++ src/main/java/mod/gravitycraft/core/GravityCraftCoreLoader.java
@@ -1,8 +1,16 @@
 package mod.gravitycraft.core;
 
+import com.google.common.collect.Sets;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.MissingModsException;
+import cpw.mods.fml.common.versioning.ArtifactVersion;
+import cpw.mods.fml.common.versioning.DefaultArtifactVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author diesieben07
@@ -10,16 +18,20 @@ import java.util.Map;
 @IFMLLoadingPlugin.Name(value = GravityCraftCoreLoader.NAME)
 @IFMLLoadingPlugin.MCVersion(value = "1.7.10")
 @IFMLLoadingPlugin.TransformerExclusions(value = {"ivorius.gravitycraft.core.", "ivorius.ivtoolkit.asm."})
-@IFMLLoadingPlugin.SortingIndex(value = 0)
+@IFMLLoadingPlugin.SortingIndex(value = 1001)
 public final class GravityCraftCoreLoader implements IFMLLoadingPlugin
 {
     public static final String MODID = "gravitycraftcore";
     public static final String NAME = "GravityCraft Core";
 
+    public static Logger logger = LogManager.getLogger(MODID);
+
 	@Override
-	public String[] getASMTransformerClass() {
-		return null;
-	}
+	public String[] getASMTransformerClass()
+    {
+        requireClass("ivorius.ivtoolkit.asm.IvClassTransformerManager", new DefaultArtifactVersion("ivtoolkit", true), MODID, NAME);
+        return new String[]{GravityCraftClassTransformer.class.getName()};
+    }
 
 	@Override
 	public String getModContainerClass() {
@@ -38,4 +50,19 @@ public final class GravityCraftCoreLoader implements IFMLLoadingPlugin
 	public String getAccessTransformerClass() {
 		return null;
 	}
+
+    public static void requireClass(String classname, ArtifactVersion artifactVersion, String modID, String modName)
+    {
+        try
+        {
+            Class.forName(classname);
+        }
+        catch (ClassNotFoundException e)
+        {
+            FMLLog.severe("The mod %s (%s) requires mods %s to be available", modID, modName, artifactVersion.getLabel());
+            Set<ArtifactVersion> versionMissingMods = Sets.newHashSet();
+            versionMissingMods.add(artifactVersion);
+            throw new MissingModsException(versionMissingMods);
+        }
+    }
 }
